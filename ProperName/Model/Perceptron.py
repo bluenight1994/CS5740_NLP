4ae3349c023e85bd1415652b5e39dd24050b8334
ganhong <1137361494@qq.com>
2017-02-16 00:14:17 -0500

    use to tf-idf transform n-gram chracter

--- ProperName/Model/Perceptron.py
+++ ProperName/Model/Perceptron.py
@@ -11,9 +11,8 @@
 
 
 import numpy as np
-from scipy.sparse import hstack
 
-class Perceptron(object):
+class Perceptron:
 
 	def __init__(self, eta = 0.01, epochs = 50, n_class = 2, dim = 0):
 		self.eta = eta
@@ -22,18 +21,20 @@ class Perceptron(object):
 		self.W = [np.zeros(1+dim) for _ in xrange(self.n_class)]
 
 	def train(self, X, Y):
-		self.X = np.append(np.ones((self.X.shape[0],1)), self.X, axis = 1)
+		self.X = np.append(np.ones((X.shape[0],1)), X, axis = 1)
 		self.Y = Y
 		seq = np.arange(self.X.shape[0])
 		np.random.shuffle(seq)
-        for _ in range(self.epochs):
+		cnt = 0
+		for _ in range(self.epochs):
 			converged = True
 			for xi, yi in zip(self.X[seq], self.Y[seq]):
 				vec = np.zeros(self.n_class)
 				for i in range(self.n_class):
 					vec[i] = np.dot(xi, self.W[i])
 				y_p = np.argmax(vec)
-				if yi == y_p: 
+				if yi == y_p:
+					cnt += 1 
 					continue
 				else: converged = False
 				for i in range(self.n_class):
@@ -42,6 +43,7 @@ class Perceptron(object):
 					else:
 						self.W[i] -= self.eta * xi
 			if converged: break
+		print "loss: " + str(float(cnt)/self.X.shape[0])
 
 
 	def predict(self, X):
